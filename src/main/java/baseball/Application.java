28ae4b4ac6918a900230308637a899ccdf7b1a6f
kimhyunjoong <100519998+khj0196@users.noreply.github.com>
2025-02-21 20:08:13 +0900

    donw

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,8 +1,14 @@
 package baseball;
 
+import baseball.computer.Computer;
+import baseball.user.EnterUserNumber;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-        
+        Computer computer = new Computer();
+        EnterUserNumber enterUserNumber = new EnterUserNumber();
+        computer.test();
+        enterUserNumber.enterUserNumber();
     }
 }
---
+++ src/main/java/baseball/computer/Computer.java
@@ -0,0 +1,8 @@
+package baseball.computer;
+
+public class Computer {
+  RandomNumber randomNumber = new RandomNumber();
+  public void test () {
+    System.out.print(randomNumber.randomNumber());
+  }
+}
